ed290106579c88d25e06c581f3cf432dba0832a7
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-07-15 21:04:24 -0300

    chore: default prettier config and formating

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,23 @@
  */
 export const TaggedError = <Tag extends string>(tag: Tag) => {
   return class TaggedErrorClass extends Error {
-    readonly _tag = tag
+    readonly _tag = tag;
 
-    constructor(
-      args?: Record<string, any>
-    ) {
-      super()
-      
-      this.name = tag
-      
+    constructor(args?: Record<string, any>) {
+      super();
+
+      this.name = tag;
+
       if (args) {
-        Object.assign(this, args)
+        Object.assign(this, args);
       }
-      
-      Object.setPrototypeOf(this, TaggedErrorClass.prototype)
-      
+
+      Object.setPrototypeOf(this, TaggedErrorClass.prototype);
     }
   } as {
-    new (): Error & { readonly _tag: Tag }
-    new <A extends Record<string, any>>(args: A): Error & { readonly _tag: Tag } & Readonly<A>
-  }
-}
+    new (): Error & { readonly _tag: Tag };
+    new <A extends Record<string, any>>(args: A): Error & {
+      readonly _tag: Tag;
+    } & Readonly<A>;
+  };
+};
